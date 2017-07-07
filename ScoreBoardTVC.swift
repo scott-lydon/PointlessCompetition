0624068075d00881f6e6223b61480c8832691145
Scott Lydon <scottlydon@Scotts-iMac.hsd1.ca.comcast.net>
2017-07-07 02:24:36 -0700

    starting to setup table

--- ScoreBoardTVC.swift
+++ ScoreBoardTVC.swift
@@ -1,24 +1,43 @@
-
-
 import UIKit
+import FirebaseDatabase
+import FirebaseAuth
 
 class ScoreBoardTVC: UITableViewController {
 
     var receivedCount = Int()
+    var highScores = DatabaseQuery()
+    var snapShot = DataSnapshot()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellID")
+        
+        highScores = Database.database().reference().child("Users").queryOrdered(byChild: "score")//.queryLimited(toLast: 10000)
+        highScores.observeSingleEvent(of: .value, with: { (snapshot) in
+            self.snapShot = snapshot
+            print(self.snapShot)
+            let alert = UIAlertController(title: "Alert", message: "leaderboard snapshot: \(snapshot)", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }, withCancel: nil)
 
+     
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-         print(receivedCount)
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 1
     }
 
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellID", for: indexPath)
+        cell.textLabel?.text = "hey"
+        return cell
+    }
 
 }
--- The Pointless Competition/ViewController.swift
+++ The Pointless Competition/ViewController.swift
@@ -12,40 +12,26 @@ class ViewController: UIViewController {
     var ref: DatabaseReference!
     let user = Auth.auth().currentUser
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         ref = Database.database().reference()
         btn.layer.cornerRadius = 107
         btn.clipsToBounds = true
-        
     }
     
-    
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let newVC: ScoreBoardTVC = segue.destination as! ScoreBoardTVC
-        newVC.receivedCount = scoreCount
-    }
     
     @IBAction func btnPress(_ sender: Any) {
+        
         scoreCount += 1
         score.text = "\(scoreCount)"
         
         let post : [String : Int] = ["Score" : scoreCount]
         self.ref.child("Users").child((user?.uid)!).setValue(post)
         
-        let highScores = Database.database().reference().child("Users").queryOrdered(byChild: "score").queryLimited(toLast: 1)
-        
-        highScores.observeSingleEvent(of: .value, with: { (snapshot) in
-            let alert = UIAlertController(title: "Alert", message: "leaderboard snapshot: \(snapshot)", preferredStyle: UIAlertControllerStyle.alert)
-            alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-        }, withCancel: nil)
-        
-        
     }
     
+    @IBAction func scoreBoardPress(_ sender: Any) {
+    }
     
     
 }//vc
